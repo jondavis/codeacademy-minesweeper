4fa794ed219181ad6d168e634be5fecd6566b823
jodavis <jdavis@shoedazzle.com>
2018-12-16 14:05:31 -0800

    completed "Day 5 - Create the Minesweeper Project"

---
+++ src/minesweeper.js
@@ -0,0 +1,13 @@
+const blankline = '  |   |  ';
+console.log('This is what an empty board would look like: ');
+console.log(blankline);
+console.log(blankline);
+console.log(blankline);
+
+const guessline = '1 |   |  ';
+const bombline = '  | B |  ';
+
+console.log('This is what a board with a guess and a bomb on it would look like:');
+console.log(guessline);
+console.log(bombline);
+console.log(blankline);
